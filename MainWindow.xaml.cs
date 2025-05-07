257bef58be9bbd84232fc1eda629bfbe62289d12
madziargoat2 <allegroaha11@gmail.com>
2025-05-07 22:37:13 +0200

    cos tam zrobilem

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,73 +1,102 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace Gra2D
 {
     public partial class MainWindow : Window
     {
-        // Stałe reprezentujące rodzaje terenu
-        public const int LAS = 1;     // las (drewno)
-        public const int LAKA = 2;    // łąka (nic)
-        public const int SKALA = 3;   // skały (kamień)
-        public const int BAGNO = 4;   // bagno (spowalnia)
-        public const int RZEKA = 5;   // rzeka (nie da się przejść)
-        public const int GORY = 6;    // góry (złoto)
-        public const int ILE_TERENOW = 7;   // ile terenów
-
-        private int[,] mapa;
-        private int szerokoscMapy;
-        private int wysokoscMapy;
-        private Image[,] tablicaTerenu;
+        public const int LAS = 1, LAKA = 2, SKALA = 3, BAGNO = 4, RZEKA = 5, GORY = 6;
         private const int RozmiarSegmentu = 32;
 
-        private BitmapImage[] obrazyTerenu = new BitmapImage[ILE_TERENOW];
-
-        // Pozycja gracza
-        private int pozycjaGraczaX = 0;
-        private int pozycjaGraczaY = 0;
+        private int[,] mapa;
+        private int szerokoscMapy = 20, wysokoscMapy = 20;
+        private Image[,] tablicaTerenu;
+        private ImageSource[] obrazyTerenu = new ImageSource[7];
         private Image obrazGracza;
+        private int pozycjaGraczaX = 10, pozycjaGraczaY = 10;
 
-        // Statystyki gracza
         public int HP { get; set; } = 100;
-        public int MaxHP { get; set; } = 100;
         public int Stamina { get; set; } = 50;
-        public int MaxStamina { get; set; } = 50;
-        public int Drewno { get; set; } = 0;
-        public int Kamien { get; set; } = 0;
-        public int Zloto { get; set; } = 0;
-
-        // Cel poziomu
-        public int CelDrewna { get; set; } = 10;
-        public int CelKamienia { get; set; } = 5;
-        public int CelZlota { get; set; } = 3;
+        public int Drewno { get; set; }
+        public int Kamien { get; set; }
+        public int Zloto { get; set; }
         public int AktualnyPoziom { get; set; } = 1;
 
-        // Lista wrogów
-        private List<Wrog> wrogowie = new List<Wrog>();
+        public int WymaganeDrewno => 10 + (AktualnyPoziom * 2);
+        public int WymaganyKamien => 5 + AktualnyPoziom;
+        public int WymaganeZloto => 3 + AktualnyPoziom;
+
+        private Random rand = new Random();
+        private DispatcherTimer gameTimer;
 
         public MainWindow()
         {
             InitializeComponent();
-            WczytajObrazyTerenu();
-            InicjalizujGracza();
-            WygenerujMape(20, 20); // Generuje mapę 20x20
+            Loaded += (s, e) => InicjalizujGre();
         }
 
-        private void WczytajObrazyTerenu()
+        private void InicjalizujGre()
         {
-            obrazyTerenu[LAS] = new BitmapImage(new Uri("las.png", UriKind.Relative));
-            obrazyTerenu[LAKA] = new BitmapImage(new Uri("laka.png", UriKind.Relative));
-            obrazyTerenu[SKALA] = new BitmapImage(new Uri("skala.png", UriKind.Relative));
-            obrazyTerenu[BAGNO] = new BitmapImage(new Uri("bagno.png", UriKind.Relative));
-            obrazyTerenu[RZEKA] = new BitmapImage(new Uri("rzeka.png", UriKind.Relative));
-            obrazyTerenu[GORY] = new BitmapImage(new Uri("gory.png", UriKind.Relative));
+            WczytajObrazy();
+            StworzGracza();
+            WygenerujMape();
+            StartTimer();
         }
 
-        private void InicjalizujGracza()
+        private void NowyPoziom()
+        {
+            AktualnyPoziom++;
+            szerokoscMapy = 20 + (AktualnyPoziom * 2);
+            wysokoscMapy = 20 + (AktualnyPoziom * 2);
+
+            // Zerowanie statystyk
+            HP = 100;
+            Stamina = 50;
+            Drewno = 0;
+            Kamien = 0;
+            Zloto = 0;
+
+            WygenerujMape();
+            AktualizujStatystyki();
+
+            MessageBox.Show(
+                $"Poziom {AktualnyPoziom}!\n\nWymagania:\n" +
+                $"Drewno: {WymaganeDrewno}\nKamień: {WymaganyKamien}\nZłoto: {WymaganeZloto}",
+                "Nowy poziom",
+                MessageBoxButton.OK);
+        }
+
+        private void WczytajObrazy()
+        {
+            string[] pliki = { "", "las.png", "laka.png", "skala.png", "bagno.png", "rzeka.png", "gory.png" };
+            for (int i = 1; i <= 6; i++)
+            {
+                try
+                {
+                    obrazyTerenu[i] = new BitmapImage(new Uri(pliki[i], UriKind.Relative));
+                }
+                catch
+                {
+                    var bmp = new WriteableBitmap(32, 32, 96, 96, PixelFormats.Bgr32, null);
+                    byte[] pixels = new byte[32 * 32 * 4];
+                    for (int j = 0; j < pixels.Length; j += 4)
+                    {
+                        pixels[j] = 0;
+                        pixels[j + 1] = 0;
+                        pixels[j + 2] = 255;
+                    }
+                    bmp.WritePixels(new Int32Rect(0, 0, 32, 32), pixels, 32 * 4, 0);
+                    obrazyTerenu[i] = bmp;
+                }
+            }
+        }
+
+        private void StworzGracza()
         {
             obrazGracza = new Image
             {
@@ -77,62 +106,43 @@ namespace Gra2D
             };
         }
 
-        private void WygenerujMape(int szerokosc, int wysokosc)
+        private void WygenerujMape()
         {
-            Random rand = new Random();
-            mapa = new int[wysokosc, szerokosc];
-            szerokoscMapy = szerokosc;
-            wysokoscMapy = wysokosc;
-            wrogowie.Clear();
-
-            // Wypełnij mapę losowymi terenami
-            for (int y = 0; y < wysokosc; y++)
-            {
-                for (int x = 0; x < szerokosc; x++)
-                {
-                    // 40% łąka, 20% las, 15% skały, 10% bagno, 10% rzeka, 5% góry
-                    int los = rand.Next(100);
-                    if (los < 40) mapa[y, x] = LAKA;
-                    else if (los < 60) mapa[y, x] = LAS;
-                    else if (los < 75) mapa[y, x] = SKALA;
-                    else if (los < 85) mapa[y, x] = BAGNO;
-                    else if (los < 95) mapa[y, x] = RZEKA;
-                    else mapa[y, x] = GORY;
-
-                    // 5% szans na wroga (tylko na łące, w lesie lub bagnie)
-                    if (rand.Next(100) < 5 && (mapa[y, x] == LAKA || mapa[y, x] == LAS || mapa[y, x] == BAGNO))
-                    {
-                        wrogowie.Add(new Wrog(x, y));
-                    }
-                }
-            }
-
-            // Upewnij się, że gracz ma wolne pole startowe (0,0)
-            mapa[0, 0] = LAKA;
-
-            // Narysuj mapę
-            RysujMape();
-        }
-
-        private void RysujMape()
-        {
+            mapa = new int[wysokoscMapy, szerokoscMapy];
+            tablicaTerenu = new Image[wysokoscMapy, szerokoscMapy];
             SiatkaMapy.Children.Clear();
             SiatkaMapy.RowDefinitions.Clear();
             SiatkaMapy.ColumnDefinitions.Clear();
 
-            // Przygotuj siatkę
-            for (int y = 0; y < wysokoscMapy; y++)
+            for (int i = 0; i < wysokoscMapy; i++)
                 SiatkaMapy.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(RozmiarSegmentu) });
-            for (int x = 0; x < szerokoscMapy; x++)
+
+            for (int i = 0; i < szerokoscMapy; i++)
                 SiatkaMapy.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(RozmiarSegmentu) });
 
-            // Wypełnij mapę obrazkami
-            tablicaTerenu = new Image[wysokoscMapy, szerokoscMapy];
             for (int y = 0; y < wysokoscMapy; y++)
             {
                 for (int x = 0; x < szerokoscMapy; x++)
                 {
-                    Image obraz = new Image
+                    int los = rand.Next(100);
+                    if (los < 40) mapa[y, x] = LAKA;
+                    else if (los < 60) mapa[y, x] = LAS;
+                    else if (los < 80) mapa[y, x] = SKALA;
+                    else if (los < 95) mapa[y, x] = BAGNO;
+                    else mapa[y, x] = GORY;
+                }
+            }
+
+            // Gwarantowane surowce
+            for (int i = 0; i < 15 + (AktualnyPoziom * 3); i++) UmiescSurowiec(LAS, SKALA, RZEKA);
+            for (int i = 0; i < 10 + (AktualnyPoziom * 2); i++) UmiescSurowiec(SKALA, SKALA, RZEKA);
+            for (int i = 0; i < 3 + AktualnyPoziom; i++) UmiescSurowiec(GORY, SKALA, RZEKA);
+
+            for (int y = 0; y < wysokoscMapy; y++)
+            {
+                for (int x = 0; x < szerokoscMapy; x++)
+                {
+                    var obraz = new Image
                     {
                         Width = RozmiarSegmentu,
                         Height = RozmiarSegmentu,
@@ -145,143 +155,146 @@ namespace Gra2D
                 }
             }
 
-            // Dodaj gracza i wrogów
+            pozycjaGraczaX = szerokoscMapy / 2;
+            pozycjaGraczaY = wysokoscMapy / 2;
             SiatkaMapy.Children.Add(obrazGracza);
-            Panel.SetZIndex(obrazGracza, 1);
-            pozycjaGraczaX = 0;
-            pozycjaGraczaY = 0;
             AktualizujPozycjeGracza();
+        }
 
-            foreach (var wrog in wrogowie)
+        private void UmiescSurowiec(int surowiec, params int[] niedozwolone)
+        {
+            int x, y, proba = 0;
+            do
             {
-                SiatkaMapy.Children.Add(wrog.Obraz);
-                Panel.SetZIndex(wrog.Obraz, 1);
-            }
+                x = rand.Next(szerokoscMapy);
+                y = rand.Next(wysokoscMapy);
+                proba++;
+            } while (proba < 100 && Array.Exists(niedozwolone, t => t == mapa[y, x]));
 
-            AktualizujStatystyki();
+            mapa[y, x] = surowiec;
         }
 
         private void AktualizujPozycjeGracza()
         {
-            Grid.SetRow(obrazGracza, pozycjaGraczaY);
-            Grid.SetColumn(obrazGracza, pozycjaGraczaX);
+            if (pozycjaGraczaY >= 0 && pozycjaGraczaY < wysokoscMapy &&
+                pozycjaGraczaX >= 0 && pozycjaGraczaX < szerokoscMapy)
+            {
+                Grid.SetRow(obrazGracza, pozycjaGraczaY);
+                Grid.SetColumn(obrazGracza, pozycjaGraczaX);
+            }
+        }
+
+        private void StartTimer()
+        {
+            gameTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(100)
+            };
+            gameTimer.Tick += (s, e) =>
+            {
+                if (Stamina < 50) Stamina++;
+                AktualizujStatystyki();
+            };
+            gameTimer.Start();
         }
 
         private void AktualizujStatystyki()
         {
-            EtykietaHP.Content = $"HP: {HP}/{MaxHP}";
-            EtykietaStamina.Content = $"Stamina: {Stamina}/{MaxStamina}";
-            EtykietaDrewna.Content = $"Drewno: {Drewno}/{CelDrewna}";
-            EtykietaKamien.Content = $"Kamień: {Kamien}/{CelKamienia}";
-            EtykietaZloto.Content = $"Złoto: {Zloto}/{CelZlota}";
+            EtykietaHP.Content = $"HP: {HP}/100";
+            EtykietaStamina.Content = $"Stamina: {Stamina}/50";
+            EtykietaDrewna.Content = $"Drewno: {Drewno}/{WymaganeDrewno}";
+            EtykietaKamien.Content = $"Kamień: {Kamien}/{WymaganyKamien}";
+            EtykietaZloto.Content = $"Złoto: {Zloto}/{WymaganeZloto}";
             EtykietaPoziom.Content = $"Poziom: {AktualnyPoziom}";
         }
 
         private void OknoGlowne_KeyDown(object sender, KeyEventArgs e)
         {
+            if (e.Key == Key.Escape)
+                Application.Current.Shutdown();
+
             int nowyX = pozycjaGraczaX;
             int nowyY = pozycjaGraczaY;
 
-            if (e.Key == Key.Up) nowyY--;
-            else if (e.Key == Key.Down) nowyY++;
-            else if (e.Key == Key.Left) nowyX--;
-            else if (e.Key == Key.Right) nowyX++;
-
-            // Sprawdź, czy można się poruszyć
-            if (nowyX >= 0 && nowyX < szerokoscMapy && nowyY >= 0 && nowyY < wysokoscMapy)
+            switch (e.Key)
             {
-                int teren = mapa[nowyY, nowyX];
-                if (teren != SKALA && teren != RZEKA) // Nie można wejść na skały/rzekę
-                {
-                    if (teren == BAGNO && Stamina >= 10) // Bagno zabiera staminę
-                        Stamina -= 10;
-                    else if (Stamina >= 5) // Normalny ruch zabiera 5 staminy
-                        Stamina -= 5;
+                case Key.Up: nowyY--; break;
+                case Key.Down: nowyY++; break;
+                case Key.Left: nowyX--; break;
+                case Key.Right: nowyX++; break;
+                case Key.E: ZbierzSurowiec(); return;
+                case Key.Space: Atakuj(); return;
+            }
 
+            if (nowyX >= 0 && nowyX < szerokoscMapy &&
+                nowyY >= 0 && nowyY < wysokoscMapy &&
+                Stamina >= 5)
+            {
+                if (mapa[nowyY, nowyX] != SKALA && mapa[nowyY, nowyX] != RZEKA)
+                {
+                    Stamina -= 5;
                     pozycjaGraczaX = nowyX;
                     pozycjaGraczaY = nowyY;
                     AktualizujPozycjeGracza();
                 }
             }
+        }
 
-            // Zbieranie surowców (E)
-            if (e.Key == Key.E)
+        private void ZbierzSurowiec()
+        {
+            int teren = mapa[pozycjaGraczaY, pozycjaGraczaX];
+            switch (teren)
             {
-                int teren = mapa[pozycjaGraczaY, pozycjaGraczaX];
-                switch (teren)
-                {
-                    case LAS:
-                        Drewno++;
-                        mapa[pozycjaGraczaY, pozycjaGraczaX] = LAKA;
-                        tablicaTerenu[pozycjaGraczaY, pozycjaGraczaX].Source = obrazyTerenu[LAKA];
-                        break;
-                    case SKALA:
-                        Kamien++;
-                        break;
-                    case GORY:
-                        Zloto++;
-                        break;
-                }
-                AktualizujStatystyki();
-                SprawdzPoziom();
+                case LAS:
+                    Drewno++;
+                    mapa[pozycjaGraczaY, pozycjaGraczaX] = LAKA;
+                    tablicaTerenu[pozycjaGraczaY, pozycjaGraczaX].Source = obrazyTerenu[LAKA];
+                    break;
+                case GORY:
+                    Zloto++;
+                    mapa[pozycjaGraczaY, pozycjaGraczaX] = LAKA;
+                    tablicaTerenu[pozycjaGraczaY, pozycjaGraczaX].Source = obrazyTerenu[LAKA];
+                    break;
+                case SKALA:
+                    Kamien++;
+                    break;
             }
+            Stamina = Math.Min(Stamina + 3, 50);
+            AktualizujStatystyki();
 
-            // Atak (Spacja)
-            if (e.Key == Key.Space && Stamina >= 15)
+            if (Drewno >= WymaganeDrewno &&
+                Kamien >= WymaganyKamien &&
+                Zloto >= WymaganeZloto)
             {
-                Stamina -= 15;
-                foreach (var wrog in wrogowie)
+                NowyPoziom();
+            }
+        }
+
+        private void Atakuj()
+        {
+            if (Stamina < 15) return;
+            Stamina -= 15;
+
+            for (int dy = -1; dy <= 1; dy++)
+            {
+                for (int dx = -1; dx <= 1; dx++)
                 {
-                    if (Math.Abs(wrog.X - pozycjaGraczaX) <= 1 && Math.Abs(wrog.Y - pozycjaGraczaY) <= 1)
+                    if (dx == 0 && dy == 0) continue;
+
+                    int x = pozycjaGraczaX + dx;
+                    int y = pozycjaGraczaY + dy;
+
+                    if (x >= 0 && x < szerokoscMapy &&
+                        y >= 0 && y < wysokoscMapy &&
+                        mapa[y, x] == SKALA)
                     {
-                        wrog.HP -= 20;
-                        if (wrog.HP <= 0)
-                        {
-                            SiatkaMapy.Children.Remove(wrog.Obraz);
-                            wrogowie.Remove(wrog);
-                            break;
-                        }
+                        mapa[y, x] = LAKA;
+                        tablicaTerenu[y, x].Source = obrazyTerenu[LAKA];
+                        Kamien++;
                     }
                 }
-                AktualizujStatystyki();
             }
-
-            // Regeneracja staminy (co sekundę +5)
-            if (Stamina < MaxStamina)
-                Stamina += 5;
-        }
-
-        private void SprawdzPoziom()
-        {
-            if (Drewno >= CelDrewna && Kamien >= CelKamienia && Zloto >= CelZlota)
-            {
-                AktualnyPoziom++;
-                MessageBox.Show($"Poziom {AktualnyPoziom} ukończony!");
-                WygenerujMape(20, 20); // Generuje nową mapę
-            }
-        }
-    }
-
-    // Klasa wroga
-    public class Wrog
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int HP { get; set; } = 50;
-        public Image Obraz { get; set; }
-
-        public Wrog(int x, int y)
-        {
-            X = x;
-            Y = y;
-            Obraz = new Image
-            {
-                Width = 32,
-                Height = 32,
-                Source = new BitmapImage(new Uri("wrog.png", UriKind.Relative))
-            };
-            Grid.SetRow(Obraz, y);
-            Grid.SetColumn(Obraz, x);
+            AktualizujStatystyki();
         }
     }
 }
